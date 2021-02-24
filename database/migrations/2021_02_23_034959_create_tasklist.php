f9d50e8a27ea1ceb153e7d6ca0a6d977bf3f7121
kousuke fukui <celestial.gn0@gmail.com>
2021-02-24 13:57:08 +0000

    修正2

--- database/migrations/2021_02_23_034959_create_tasklist.php
+++ database/migrations/2021_02_23_034959_create_tasklist.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasklist extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -27,6 +27,6 @@ class CreateTasklist extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('create_tasks_table');
     }
 }
